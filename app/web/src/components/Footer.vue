62354f2dcf1c4f20d5e3f2cf900acfefa0ee16ae
vmwarevirtual <vmwarevirtual@gmail.com>
2019-02-08 09:03:38 +0800

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Boot Camp AKS Yo Yo !!!!!!!!
+        Boot Camp AKS Yo Yo ------VVV
       </div>
       <div class="col-lg-6">
       </div>
